1f5e025b5e70a7a149b7b119faaa62ac361f55dd
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-09-09 17:46:16 -0300

    Added user auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,7 @@ Route::get('/', function ()
 });
 
 Route::get('/threads', 'ThreadsController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
