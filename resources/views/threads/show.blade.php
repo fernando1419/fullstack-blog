1c8b91136117f2036a4561b625ab8bd212cb001e
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-09-13 21:00:44 -0300

    Created form for posting replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -22,5 +22,23 @@
       </div>
    </div>
 
+   @if (auth()->check())
+      <div class="row justify-content-center mt-3">
+         <div class="col-md-8">
+            <form method="POST" action="{{ $thread->_path() . '/replies' }}">
+               @csrf
+               <div class="form-group">
+                  <div class="form-group">
+                     <textarea class="form-control" name="body" id="body" rows="4" placeholder="Enter your comment here.."></textarea>
+                  </div>
+                  <button type="submit" class="btn btn-primary btn-sm">Post your comment</button>
+               </div>
+            </form>
+         </div>
+      </div>
+   @else
+      <p class="text-center mt-3">Please <a href="{{ route('login') }}"> sign in</a> to participate in this thread </p>
+   @endif
+
 </div>
 @endsection
