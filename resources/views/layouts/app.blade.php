4e06c75305f657b3d05d0ec19493c4641fc2344d
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-09-16 21:27:07 -0300

    Using view composers to share the $channel variable with all views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
                         </a>
 
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                           @foreach (App\Channel::all() as $channel)
+                           @foreach ($channels as $channel)
                               <a class="dropdown-item" href="/threads/{{$channel->slug}}">{{ $channel->name }}</a>
                            @endforeach
                         </div>
